0531fa85327bde02c3d2fdb0497f5d5a0e1f65c3
Omar Zuniga <miguel_omar30@hotmail.com>
2025-04-21 23:09:26 -0600

    Margin Ruler

--- src/app/documents/[documentId]/ruler.tsx
+++ src/app/documents/[documentId]/ruler.tsx
@@ -18,26 +18,64 @@ export const Ruler = () => {
 		setIsDraggignRight(true);
 	};
 
-	{
-		/* TIME: 3:50:22*/
-	}
+	const handleMouseMove = (e: React.MouseEvent) => {
+		const PAGE_WIDTH = 816;
+		const MINIMUM_SPACE = 100;
+
+		if ((isDraggingLeft || isDraggingRight) && rulerRef.current) {
+			const container = rulerRef.current.querySelector("#ruler-container");
+			if (container) {
+				const containerRect = container.getBoundingClientRect();
+				const relativeX = e.clientX - containerRect.left;
+				const rawPosition = Math.max(0, Math.min(PAGE_WIDTH, relativeX));
+
+				if (isDraggingLeft) {
+					const maxLeftPosition = PAGE_WIDTH - rightMargin - MINIMUM_SPACE;
+					const newLeftPosition = Math.min(rawPosition, maxLeftPosition);
+					setLeftMargin(newLeftPosition); // TODO: Make collaborative
+				} else if (isDraggingRight) {
+					const maxRightPosition = PAGE_WIDTH - (leftMargin + MINIMUM_SPACE);
+					const newRightPosition = Math.max(PAGE_WIDTH - rawPosition, 0);
+					const constrainedRightPosition = Math.min(newRightPosition, maxRightPosition);
+					setRightMargin(constrainedRightPosition);
+				}
+			}
+		}
+	};
+
+	const handleMouseUp = () => {
+		setIsDragginLeft(false);
+		setIsDraggignRight(false);
+	};
+
+	const handleLeftDoubleClick = () => {
+		setLeftMargin(56);
+	};
+	const handleRightDoubleClick = () => {
+		setRightMargin(56);
+	};
 
 	return (
-		<div className="relative mx-auto flex h-6 select-none items-end border-b border-gray-300 print:hidden">
+		<div
+			ref={rulerRef}
+			onMouseMove={handleMouseMove}
+			onMouseUp={handleMouseUp}
+			onMouseLeave={handleMouseUp}
+			className="relative mx-auto flex h-6 select-none items-end border-b border-gray-300 print:hidden">
 			<div id="ruler-container" className="max-w-[816px] mx-auto w-full h-full relative">
 				<Marker
-					position={56}
+					position={leftMargin}
 					isLeft={true}
-					isDragging={false}
-					onMouseDown={() => {}}
-					onDoubleClick={() => {}}
+					isDragging={isDraggingLeft}
+					onMouseDown={handleLeftMouseDown}
+					onDoubleClick={handleLeftDoubleClick}
 				/>
 				<Marker
-					position={56}
+					position={rightMargin}
 					isLeft={false}
-					isDragging={false}
-					onMouseDown={() => {}}
-					onDoubleClick={() => {}}
+					isDragging={isDraggingRight}
+					onMouseDown={handleRightMouseDown}
+					onDoubleClick={handleRightDoubleClick}
 				/>
 				<div className="absolute inset-x-0 bottom-0 h-full">
 					<div className="relative h-full w-[816px]">
@@ -86,6 +124,16 @@ const Marker = ({ position, isLeft, isDragging, onMouseDown, onDoubleClick }: Ma
 			onMouseDown={onMouseDown}
 			onDoubleClick={onDoubleClick}>
 			<FaCaretDown className="absolute left-1/2 top-0 h-full fill-blue-500 transform -translate-x-1/2" />
+			<div
+				className="absolute left-1/2 top-4 -translate-x-1/2"
+				style={{
+					height: "100vh",
+					width: "1px",
+					transform: "scaleX(0.5)",
+					backgroundColor: "#3b72f6",
+					display: isDragging ? "block" : "none",
+				}}
+			/>
 		</div>
 	);
 };
